7426055008b474627cef4eae77c122b699a42462
Sae Woo Nam <nams@nist.gov>
2022-03-17 09:27:05 -0600

    add all files

---
+++ gh-pages.js
@@ -0,0 +1,19 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+	'public', // path to public directory
+	{
+		// branch: 'gh-pages',
+		branch: 'docs',
+            repo: 'git@github.com:saewoonam/svelte-pages.git',
+		// repo: 'https://github.com/saewoonam/svelte-pages.git', // Update to point to your repository
+		user: {
+			name: 'Saewoo', // update to use your name
+			email: 'nams@nist.gov' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { onMount } from 'svelte';
+	export let name;
+    onMount(async() => {
+        console.log('try to fetch');
+        let response = await fetch('https://raw.githubusercontent.com/saewoonam/svelte-pages/main/src/App.svelte');
+        let t = await response.text()
+        console.log('results:', t);
+    });
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
